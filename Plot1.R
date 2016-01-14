a561bcb2faa108de7f6442fa74ddef1602a8974d
mnvnavya <nnaveen126@yahoo.co.in>
2016-01-14 09:26:48 -0500

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -24,6 +24,9 @@ tail(newdataset)
 plot1 <- hist(as.numeric(newdataset$Global_active_power), main = "Global Active POwer",xlab = "Global Active Power(kilowatts)", col = "Red")
 plot1
 
+# saving image
+dev.copy(png, file="plot1.png", height=480, width=480)
+dev.off()
 
 
 
